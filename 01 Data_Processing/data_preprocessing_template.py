9ef4bb37604a6f837de0197c3be00fee1480675b
Carlos <carlos.2380@hotmail.com>
2018-11-28 02:17:26 +0100

    Scaling data

--- 01 Data_Processing/data_preprocessing_template.py
+++ 01 Data_Processing/data_preprocessing_template.py
@@ -20,11 +20,18 @@ X[:,1:3] = imputer.transform(X[:,1:3])
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-oneHotEncoder = OneHotEncoder(categoriacl_features = [0])
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
 X = oneHotEncoder.fit_transform(X).toarray()
 labelencoder_Y = LabelEncoder()
 y = labelencoder_Y.fit_transform(y)
 
 #Splitting dataset into Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test
+####random_state is the seed of random, is optional only for educational code
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0) 
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test) #We use sc_X in both for keep the scale
