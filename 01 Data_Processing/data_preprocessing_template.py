a606c1ce441e8023399912bafa1fb2eba2a67ae2
Carlos <carlos.2380@hotmail.com>
2018-11-28 01:46:35 +0100

    Encoding Categoriacal data

--- 01 Data_Processing/data_preprocessing_template.py
+++ 01 Data_Processing/data_preprocessing_template.py
@@ -15,3 +15,8 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean',axis = 0)
 imputer = imputer.fit(x[:,1:3])
 x[:,1:3] = imputer.transform(x[:,1:3])
+
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder
+labelencoder_X = LabelEncoder()
+labelencoder_X.fit.transform([:, 0])
