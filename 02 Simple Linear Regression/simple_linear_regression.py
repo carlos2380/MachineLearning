a5ded0bba41444fbdb7e6208669835d944946712
Carlos <carlos.2380@hotmail.com>
2018-11-28 04:20:24 +0100

    Predictiong test result

--- 02 Simple Linear Regression/simple_linear_regression.py
+++ 02 Simple Linear Regression/simple_linear_regression.py
@@ -1,1 +1,25 @@
+# Simple Linear Regression
 
+# Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting dataset into Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0) 
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
