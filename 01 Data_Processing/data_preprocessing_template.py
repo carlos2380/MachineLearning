d6f3b383dbf317878bd317804cf9e7e0a94eeb27
Carlos <carlos.2380@hotmail.com>
2018-11-28 02:01:26 +0100

    Split training and test data

--- 01 Data_Processing/data_preprocessing_template.py
+++ 01 Data_Processing/data_preprocessing_template.py
@@ -7,16 +7,24 @@ import pandas as pd
 
 #Importing the dataset
 dataset = pd.read_csv('Data.csv')
-x = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 #Taking care of missing data, take info of Imputer with ctrl+I in anaconda
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean',axis = 0)
-imputer = imputer.fit(x[:,1:3])
-x[:,1:3] = imputer.transform(x[:,1:3])
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
 
 #Encoding categorical data
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
-labelencoder_X.fit.transform([:, 0])
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+oneHotEncoder = OneHotEncoder(categoriacl_features = [0])
+X = oneHotEncoder.fit_transform(X).toarray()
+labelencoder_Y = LabelEncoder()
+y = labelencoder_Y.fit_transform(y)
+
+#Splitting dataset into Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test
